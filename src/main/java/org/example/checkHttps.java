e5e770a87af2ca0cdf529e3f1d082109b1c4bffb
melvinstark3 <kartik3163@gmail.com>
2025-06-05 12:54:52 +0530

    User Guest OrderFlow for checkhttps

--- src/main/java/org/example/checkHttps.java
+++ src/main/java/org/example/checkHttps.java
@@ -1,20 +1,12 @@
 package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class checkHttps extends Main{
+public class checkHttps extends guestOrder{
     public static void main(String[] args) throws InterruptedException {
         invokeBrowser();
-        driver.navigate().to("https://gateway.demo-ordering.online/");
-        driver.findElement(By.xpath("//button[@data-testid=\"modeSelect2\"]")).click();
-        createCart("First Location","Mama-Mia");
-        wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@data-testid=\"paymentMode1\"]")));
-        //Select Payment method (paymentMode0 = 1st - COD , paymentMode1 = 2nd - Online)
-        driver.findElement(By.xpath("//input[@data-testid=\"paymentMode1\"]")).click();
-        driver.findElement(By.xpath("(//button[@data-testid=\"placeOrder\"])[2]")).click();
+        orderFlow();
         checkHttps();
     }
 }
