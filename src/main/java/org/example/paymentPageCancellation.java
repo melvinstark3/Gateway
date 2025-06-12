b0cef6e4f25f48f94b4b6269e24f72d8480aceb4
melvinstark3 <kartik3163@gmail.com>
2025-06-12 14:33:40 +0530

    Add Case Number for Payment Cancellation

--- src/main/java/org/example/paymentPageCancellation.java
+++ src/main/java/org/example/paymentPageCancellation.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Objects;
+
 public class paymentPageCancellation extends browserSetup{
 
     public paymentPageCancellation() throws InterruptedException {
@@ -14,9 +16,9 @@ public class paymentPageCancellation extends browserSetup{
         driver.findElement(By.id("back-button")).click();
         try {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cart-header"))).isDisplayed();
-            System.out.println("User is redirected to Menu");
+            System.out.println("TC 3: PASS: After Payment Cancellation User is redirected to Menu");
         } catch (NoSuchElementException | TimeoutException e) {
-            System.out.println("User is not redirected to Menu");
+            System.out.println("TC 3: FAIL: After Payment Cancellation User is not redirected to Menu");
         }
         System.out.println("Cancellation of Payment takes User to the URL: " + driver.getCurrentUrl());
     }
