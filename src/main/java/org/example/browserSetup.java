36f4bc49b890ae504da1531abcd509b4b925a28d
Kartik <kartik@restolabs.com>
2025-07-21 15:43:46 +0530

    Update ChromeDriver Prefs

--- src/main/java/org/example/browserSetup.java
+++ src/main/java/org/example/browserSetup.java
@@ -2,9 +2,12 @@ package org.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class browserSetup {
@@ -15,14 +18,21 @@ public class browserSetup {
 
     public static void invokeBrowser() {
         System.setProperty("webdriver.chrome.driver", "/Users/kartik/Documents/bin/chromedriver");
-        driver = new ChromeDriver();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
     public static String readProperty(String key) {
         Properties prop = new Properties();
         try {
-            FileInputStream fis = new FileInputStream("/Users/kartik/Desktop/Gateway/src/main/resources/config.properties");
+            FileInputStream fis = new FileInputStream("/Users/kartik/Desktop/restolabs_testing/src/main/resources/config.properties");
             prop.load(fis);
         } catch (Exception e) {
             System.out.println(e.getMessage());
